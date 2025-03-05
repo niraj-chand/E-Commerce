133ca56c10857142fa1846d3b8f9460700f3a0e0
blissexodia <binnoley@gmail.com>
2025-03-05 23:32:15 +0545

    still not working btw

--- client/src/components/ui/separator.jsx
+++ client/src/components/ui/separator.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/lib/utils"
-
+//sep
 const Separator = React.forwardRef((
   { className, orientation = "horizontal", decorative = true, ...props },
   ref
