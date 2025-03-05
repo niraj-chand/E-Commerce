0ec52f46d86069adf9c485fb27efda699c2e669a
blissexodia <binnoley@gmail.com>
2025-03-05 23:30:48 +0545

    inputs

--- client/src/components/ui/label.jsx
+++ client/src/components/ui/label.jsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
-
+//input
 const Label = React.forwardRef(({ className, ...props }, ref) => (
   <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
 ))
