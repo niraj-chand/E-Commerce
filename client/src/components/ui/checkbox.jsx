28773f1ac15a73f35a750ebcf6a64a57f1de4ab2
blissexodia <binnoley@gmail.com>
2025-03-05 23:23:09 +0545

    dawg what

--- client/src/components/ui/checkbox.jsx
+++ client/src/components/ui/checkbox.jsx
@@ -3,7 +3,7 @@ import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { Check } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-//utilities
+
 const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
